6a057f848980393380f9e95306280ea9b4bc0a1a
pi <pi@raspberry>
2021-08-24 17:48:47 +1200

    Tested query

--- Sql.py
+++ Sql.py
@@ -1,14 +1,15 @@
 """This module controls interacting with an external MySQL server"""
 
+import sys
 import mysql.connector
 
 class BrewingDatabase:
-	def __init__(self, username = 'admin', password = 'password', host = '10.1.1.10', database = 'brewing'):
-		self.username = username
-		self.password = password
-		self.host = host
-		self.database = database
-		self.brewID = None
+	def __init__(self, username = 'brewing_pi', host = '10.1.1.10', database = 'brewing'):
+		self._username = username
+		self._host = host
+		self._database = database
+		self._brewID = None
+		self._connection = None
 	
 	def __enter__(self):
 		"""
@@ -16,42 +17,48 @@ class BrewingDatabase:
 		   This function raises mysql.connection.Error if it is unable to connect
 		"""
 		try:
-			self.connection = mysql.connector.connect(
-				user = self.username,
-				password = self.password,
-				host = self.host,
-				database = self.database)
+			self._connection = mysql.connector.connect(
+				user = self._username,
+				host = self._host,
+				database = self._database,
+				charset = 'utf8',
+				collation = 'utf8_general_ci')
 			
-			self._brewID, self.setpoint = self._readCurrentBrew()
+			currentBrew = self._readCurrentBrew()
+			if currentBrew != None:
+				self._brewID, self._setpoint = currentBrew
+			
+			return self
 		except:
 			self.__exit__(*sys.exc_info())
 			raise
 	
 	def __exit__(self, exception_type, exception_value, exception_trace):
 		"""Cleanup from the 'with' statement"""
-		self.connection.close()
+		if self._connection != None:
+			self._connection.close()
 	
 	def isBrewRunning(self):
 		return self._brewID != None
 	
 	def setpoint(self):
-		return self.setpoint
+		return self._setpoint
 	
 	def _readCurrentBrew(self):
 		"""Reads the current brewing setpoint, this will return None if the brewing is not currently enabled"""
 		try:
-			cursor = self.connection.cursor()
-			query = "SELECT id, set_point FROM brews WHERE end_time IS NOT NULL"
+			cursor = self._connection.cursor()
+			query = "SELECT id, set_point FROM brews WHERE end_time IS NULL"
 			cursor.execute(query)
 			return cursor.fetchone()
 		finally:
 			cursor.close()
 	
-	def writeCurrentStatus(self, temperature, is_heating):
+	def writeCurrentStatus(self, temperature, isHeating):
 		"""Writes the temperature and wether the heating mat is on to the database"""
 		try:
-			cursor = self.connection.cursor()
-			query = "INSERT INTO timeseries SELECT %s, %s, %s"
-			cursor.execute(query)
+			cursor = self._connection.cursor()
+			query = "INSERT INTO timeseries (id, temperature, set_point, is_heating) VALUES (%s, %s, %s, %s)"
+			cursor.execute(query, (self._brewID, temperature, self.setpoint, isHeating))
 		finally:
 			cursor.close()
